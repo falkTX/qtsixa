36cd7e06ef8e419de486e4843f57e2df6bc7fb32
falkTX <falktx@gmail.com>
2011-04-24 19:22:24 +0100

    Fix repeated keys

--- sixad/remote.cpp
+++ sixad/remote.cpp
@@ -24,7 +24,13 @@
 #include <unistd.h>
 
 int b1, b2, b3;
-int last_b1 = 0xff;
+int last_jb1 = 0;
+int last_jb2 = 0;
+int last_jb3 = 0;
+int last_rb1 = 0xff;
+int last_ib1 = 0;
+int last_ib2 = 0;
+int last_ib3 = 0;
 int last_key = -1;
 
 static int remote2key(int key, int modes) {
@@ -134,44 +140,54 @@ void do_joystick(int fd, unsigned char* buf, struct dev_joystick joystick)
 
     if (joystick.buttons) {
         //part1
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 0, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 1, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 2, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 3, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 4, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 5, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 6, b1 & 1);
-        b1 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 7, b1 & 1);
+        if (last_jb1 != b1) {
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 0, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 1, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 2, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 3, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 4, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 5, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 6, b1 & 1);
+            b1 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 7, b1 & 1);
+        }
         //part2
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 8, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 9, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 10, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 11, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 12, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 13, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 14, b2 & 1);
-        b2 >>= 1;
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 15, b2 & 1);
+        if (last_jb2 != b2) {
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 8, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 9, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 10, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 11, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 12, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 13, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 14, b2 & 1);
+            b2 >>= 1;
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 15, b2 & 1);
+        }
         //part3
-        uinput_send(fd, EV_KEY, BTN_JOYSTICK + 16, b3 & 1);
+        if (last_jb3 != b3) {
+            uinput_send(fd, EV_KEY, BTN_JOYSTICK + 16, b3 & 1);
+        }
 
         if (b1 > 0 || b2 > 0 || b3 > 0) {
           set_active(true);
         }
     }
+
+    last_jb1 = b1;
+    last_jb2 = b2;
+    last_jb3 = b3;
 
     uinput_send(fd, EV_SYN, SYN_REPORT, 0);
 }
@@ -180,7 +196,7 @@ void do_remote(int fd, unsigned char* buf, int modes)
 {
     b1 = buf[5];
 
-    if (last_b1 != b1) {
+    if (last_rb1 != b1) {
         int x = 0;
         if (b1 != 0xff) {
             last_key = remote2key(b1, modes);
@@ -192,7 +208,7 @@ void do_remote(int fd, unsigned char* buf, int modes)
         set_active(true);
     }
 
-    last_b1 = b1;
+    last_rb1 = b1;
     uinput_send(fd, EV_SYN, SYN_REPORT, 0);
 }
 
@@ -203,43 +219,53 @@ void do_input(int fd, unsigned char* buf, struct dev_input input)
     b3 = buf[4];
 
     //part1
-    if (input.key_select) uinput_send(fd, EV_KEY, input.key_select, b1 & 1);
-    b1 >>= 1;
-    if (input.key_l3) uinput_send(fd, EV_KEY, input.key_l3, b1 & 1);
-    b1 >>= 1;
-    if (input.key_r3) uinput_send(fd, EV_KEY, input.key_r3, b1 & 1);
-    b1 >>= 1;
-    if (input.key_start) uinput_send(fd, EV_KEY, input.key_start, b1 & 1);
-    b1 >>= 1;
-    if (input.key_up) uinput_send(fd, EV_KEY, input.key_up, b1 & 1);
-    b1 >>= 1;
-    if (input.key_right) uinput_send(fd, EV_KEY, input.key_right, b1 & 1);
-    b1 >>= 1;
-    if (input.key_down) uinput_send(fd, EV_KEY, input.key_down, b1 & 1);
-    b1 >>= 1;
-    if (input.key_left) uinput_send(fd, EV_KEY, input.key_left, b1 & 1);
+    if (last_ib1 != b1) {
+        if (input.key_select) uinput_send(fd, EV_KEY, input.key_select, b1 & 1);
+        b1 >>= 1;
+        if (input.key_l3) uinput_send(fd, EV_KEY, input.key_l3, b1 & 1);
+        b1 >>= 1;
+        if (input.key_r3) uinput_send(fd, EV_KEY, input.key_r3, b1 & 1);
+        b1 >>= 1;
+        if (input.key_start) uinput_send(fd, EV_KEY, input.key_start, b1 & 1);
+        b1 >>= 1;
+        if (input.key_up) uinput_send(fd, EV_KEY, input.key_up, b1 & 1);
+        b1 >>= 1;
+        if (input.key_right) uinput_send(fd, EV_KEY, input.key_right, b1 & 1);
+        b1 >>= 1;
+        if (input.key_down) uinput_send(fd, EV_KEY, input.key_down, b1 & 1);
+        b1 >>= 1;
+        if (input.key_left) uinput_send(fd, EV_KEY, input.key_left, b1 & 1);
+    }
     //part2
-    if (input.key_l2) uinput_send(fd, EV_KEY, input.key_l2, b2 & 1);
-    b2 >>= 1;
-    if (input.key_r2) uinput_send(fd, EV_KEY, input.key_r2, b2 & 1);
-    b2 >>= 1;
-    if (input.key_l1) uinput_send(fd, EV_KEY, input.key_l1, b2 & 1);
-    b2 >>= 1;
-    if (input.key_r1) uinput_send(fd, EV_KEY, input.key_r1, b2 & 1);
-    b2 >>= 1;
-    if (input.key_tri) uinput_send(fd, EV_KEY, input.key_tri, b2 & 1);
-    b2 >>= 1;
-    if (input.key_cir) uinput_send(fd, EV_KEY, input.key_cir, b2 & 1);
-    b2 >>= 1;
-    if (input.key_cro) uinput_send(fd, EV_KEY, input.key_cro, b2 & 1);
-    b2 >>= 1;
-    if (input.key_squ) uinput_send(fd, EV_KEY, input.key_squ, b2 & 1);
+    if (last_ib2 != b2) {
+        if (input.key_l2) uinput_send(fd, EV_KEY, input.key_l2, b2 & 1);
+        b2 >>= 1;
+        if (input.key_r2) uinput_send(fd, EV_KEY, input.key_r2, b2 & 1);
+        b2 >>= 1;
+        if (input.key_l1) uinput_send(fd, EV_KEY, input.key_l1, b2 & 1);
+        b2 >>= 1;
+        if (input.key_r1) uinput_send(fd, EV_KEY, input.key_r1, b2 & 1);
+        b2 >>= 1;
+        if (input.key_tri) uinput_send(fd, EV_KEY, input.key_tri, b2 & 1);
+        b2 >>= 1;
+        if (input.key_cir) uinput_send(fd, EV_KEY, input.key_cir, b2 & 1);
+        b2 >>= 1;
+        if (input.key_cro) uinput_send(fd, EV_KEY, input.key_cro, b2 & 1);
+        b2 >>= 1;
+        if (input.key_squ) uinput_send(fd, EV_KEY, input.key_squ, b2 & 1);
+    }
     //part3
-    if (input.key_ps) uinput_send(fd, EV_KEY, input.key_ps, b3 & 1);
+    if (last_ib3 != b3) {
+        if (input.key_ps) uinput_send(fd, EV_KEY, input.key_ps, b3 & 1);
+    }
 
     if (b1 > 0 || b2 > 0 || b3 > 0) {
       set_active(true);
     }
+
+    last_ib1 = b1;
+    last_ib2 = b2;
+    last_ib3 = b3;
 
     uinput_send(fd, EV_SYN, SYN_REPORT, 0);
 }
